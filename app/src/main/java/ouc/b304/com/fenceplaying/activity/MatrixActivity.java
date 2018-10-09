45f4fac1d6d25a1ebca215543e540587cb70d192
王海峰 <1161685903@qq.com>
2018-10-09 16:14:41 +0800

    1.顺利完成单点训练的数据依次显示。 2.当次训练完成以后或者点击结束以后，直接点击开始训练可以直接重新开始新一轮训练。

---
+++ app/src/main/java/ouc/b304/com/fenceplaying/activity/MatrixActivity.java
@@ -0,0 +1,35 @@
+package ouc.b304.com.fenceplaying.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * @author 王海峰 on 2018/10/9 16:09
+ */
+public class MatrixActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
